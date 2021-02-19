ef97ed91664a74c17a3995b124e69d09784f39ef
Danny Rottstegge <danny@rottstegge.net>
2021-02-19 20:33:51 +0100

    First in project

---
+++ restmenu-server-webapp/src/main/java/de/rottstegge/restmenu/RestmenuServerWebappApplication.java
@@ -0,0 +1,13 @@
+package de.rottstegge.restmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestmenuServerWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestmenuServerWebappApplication.class, args);
+	}
+
+}
---
+++ restmenu-server-webapp/src/test/java/de/rottstegge/restmenu/RestmenuServerWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rottstegge.restmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestmenuServerWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
