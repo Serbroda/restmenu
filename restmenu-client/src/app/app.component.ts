3725b770ba0f319ec40e3064d8cb94feecea1901
Danny Rottstegge <danny@rottstegge.net>
2021-02-20 12:13:18 +0100

    Remove obsolete unused import

--- restmenu-client/src/app/app.component.ts
+++ restmenu-client/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import {Component, HostBinding, ViewChild} from '@angular/core';
 import { Plugins } from '@capacitor/core';
 import {MatSidenav} from '@angular/material/sidenav';
-import {HttpClient} from "@angular/common/http";
-import { GreetingService } from 'restmenu-server-sdk-angular';
 
 @Component({
   selector: 'app-root',
