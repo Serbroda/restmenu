c5affc99dfa7cf489ff8fe3f1a8138d6e3c0fc03
Danny Rottstegge <danny@rottstegge.net>
2021-02-20 16:44:28 +0100

    Add resource handler to main index.html if route not known

---
+++ restmenu-webapp/src/main/java/de/rottstegge/restmenu/configuration/WebConfiguration.java
@@ -0,0 +1,28 @@
+package de.rottstegge.restmenu.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**/*")
+                .addResourceLocations("classpath:/public/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestResource = location.createRelative(resourcePath);
+                        return requestResource.exists() && requestResource.isReadable() ? requestResource : new ClassPathResource("/public/index.html");
+                    }
+                });
+    }
+}
