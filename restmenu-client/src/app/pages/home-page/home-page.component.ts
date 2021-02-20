120caa5519e4a7cc6617659532acff6ac385e0a4
Danny Rottstegge <danny@rottstegge.net>
2021-02-20 01:06:31 +0100

    Rename project to have mono repo

--- restmenu-client/src/app/pages/home-page/home-page.component.ts
+++ restmenu-client/src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { GreetingService } from '../../../../../restmenu-server-sdk-angular/target/generated-sources';
+import { GreetingService } from '../../../../../restmenu-sdk-angular/target/generated-sources';
 
 @Component({
   selector: 'app-home-page',
