62df264af2ec5fbe33c5544c76736d2d9ae2080c
Danny Rottstegge <danny@rottstegge.net>
2021-02-28 13:28:22 +0100

    Add password encoder

--- restmenu-webapp/src/main/java/de/rottstegge/restmenu/configuration/SecurityConfiguration.java
+++ restmenu-webapp/src/main/java/de/rottstegge/restmenu/configuration/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package de.rottstegge.restmenu.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
@@ -18,5 +21,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .permitAll();
 
         http.headers().frameOptions().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
